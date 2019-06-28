9a66de666ceef398e2c2d6391c8cd5bfeea00804
Rohith <rkarkala10@gmail.com>
2019-06-27 19:49:32 -0500

    joke done

---
+++ commands/joke.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const package = require("../package.json");
+const axios = require('axios');
+
+module.exports.run = async (bot, message, args) => {
+    let json = await axios.get('https://official-joke-api.appspot.com/random_joke');
+    console.log(json.data);
+    let svinfo = new Discord.RichEmbed()
+        .setTitle(`${json.data.setup}`)
+        .setColor("#418ff4")
+        .addField("Answer", json.data.punchline);  
+  return message.channel.send(svinfo); //Sends server info
+};
+
+module.exports.help = {
+  name: "joke"
+};
