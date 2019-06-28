b297d90e6aae99cd4077f8748fa08b0c732df99c
Anirudh Emmadi <kanna6501@gmail.com>
2019-06-27 19:50:07 -0500

    wit api is failing

--- commands/voice.js
+++ commands/voice.js
@@ -1,15 +1,46 @@
 const Discord = require("discord.js");
 const ffmpeg = require("ffmpeg-binaries");
+const WitSpeech = require("node-witai-speech");
+const API_KEY = process.env.WIT_API;
+const content_type = "audio/wav";
 
 module.exports.run = async (bot, message, args) => {
   const connection = await message.member.voiceChannel.join();
+  const receiver = connection.createReceiver();
 
   connection.on("speaking", (user, speaking) => {
     if (speaking) {
       console.log(`I'm listening to ${user.username}`);
-    } else {
-      console.log(`I stopped listening to ${user.username}`);
     }
+    const audioStream = receiver.createPCMStream(user);
+    const requestConfig = {
+      encoding: "LINEAR16",
+      sampleRateHertz: 48000,
+      languageCode: "en-US"
+    };
+    const request = {
+      config: requestConfig
+    };
+
+    const parseSpeech = new Promise((resolve, reject) => {
+      WitSpeech.extractSpeechIntent(
+        API_KEY,
+        audioStream,
+        content_type,
+        (err, res) => {
+          if (err) return reject(err);
+          resolve(res);
+        }
+      );
+    });
+
+    parseSpeech
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   });
 };
 
